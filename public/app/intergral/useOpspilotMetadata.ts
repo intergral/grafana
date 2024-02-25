a75bd0f27052ef6d0e1d77f7070c11a8a991ddee
Glen Dovey <8122900+glend1@users.noreply.github.com>
2024-02-25 17:33:47 +0000

    CHORE: remove screenshots (#11)
    Co-authored-by: Glen Dovey <glen.dovey@intergral.com>

--- public/app/intergral/useOpspilotMetadata.ts
+++ public/app/intergral/useOpspilotMetadata.ts
@@ -1,4 +1,3 @@
-import domtoimage from 'dom-to-image';
 import { useEffect } from 'react';
 
 import { getTimeSrv } from 'app/features/dashboard/services/TimeSrv';
@@ -8,13 +7,11 @@ export const useOpspilotMetadata = () => {
     useEffect(() => {
         const event = async (event: MessageEvent) => {
           if (event.data.type === 'opspilot-host.getMetadata') {
-            const screenshot = await domtoimage.toPng(document.querySelector('main') as HTMLElement, { bgcolor: '#000' });
             window.parent.postMessage({type: "opspilot-slave.sendMetadata", metadata: { 
-              url: window.location.pathname,
+              slaveUrl: window.location.pathname,
               timeStart: getTimeSrv().timeRange().from.valueOf(), 
               timeEnd: getTimeSrv().timeRange().to.valueOf(),
               timezone: getTimeSrv().timeModel?.getTimezone() === 'browser' ? Intl.DateTimeFormat().resolvedOptions().timeZone : getTimeSrv().timeModel?.getTimezone(),
-              screenshot: screenshot
             } }, '*');
           }
         };
