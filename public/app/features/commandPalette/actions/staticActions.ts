c65615c4a0139ed94424c6eff4748621b53b1aee
Dan Hodgson <daniel.hodgson@intergral.com>
2024-06-14 10:22:47 +0100

    comment out code to remove errors

--- public/app/features/commandPalette/actions/staticActions.ts
+++ public/app/features/commandPalette/actions/staticActions.ts
@@ -1,12 +1,12 @@
 import { NavModelItem } from '@grafana/data';
 import { enrichHelpItem } from 'app/core/components/AppChrome/MegaMenu/utils';
 import { t } from 'app/core/internationalization';
-import { changeTheme } from 'app/core/services/theme';
+// import { changeTheme } from 'app/core/services/theme';
 
 import { CommandPaletteAction } from '../types';
-import { ACTIONS_PRIORITY, DEFAULT_PRIORITY, PREFERENCES_PRIORITY } from '../values';
+import { ACTIONS_PRIORITY, DEFAULT_PRIORITY } from '../values';
 
-import getExtensionActions from './extensionActions';
+// import getExtensionActions from './extensionActions';
 
 // TODO: Clean this once ID is mandatory on nav items
 function idForNavItem(navItem: NavModelItem) {
@@ -73,33 +73,33 @@ function navTreeToActions(navTree: NavModelItem[], parents: NavModelItem[] = [])
 }
 
 export default (navBarTree: NavModelItem[]): CommandPaletteAction[] => {
-  const globalActions: CommandPaletteAction[] = [
-    {
-      id: 'preferences/theme',
-      name: t('command-palette.action.change-theme', 'Change theme...'),
-      keywords: 'interface color dark light',
-      section: t('command-palette.section.preferences', 'Preferences'),
-      priority: PREFERENCES_PRIORITY,
-    },
-    {
-      id: 'preferences/dark-theme',
-      name: t('command-palette.action.dark-theme', 'Dark'),
-      keywords: 'dark theme',
-      perform: () => changeTheme('dark'),
-      parent: 'preferences/theme',
-      priority: PREFERENCES_PRIORITY,
-    },
-    {
-      id: 'preferences/light-theme',
-      name: t('command-palette.action.light-theme', 'Light'),
-      keywords: 'light theme',
-      perform: () => changeTheme('light'),
-      parent: 'preferences/theme',
-      priority: PREFERENCES_PRIORITY,
-    },
-  ];
+  // const globalActions: CommandPaletteAction[] = [
+  //   {
+  //     id: 'preferences/theme',
+  //     name: t('command-palette.action.change-theme', 'Change theme...'),
+  //     keywords: 'interface color dark light',
+  //     section: t('command-palette.section.preferences', 'Preferences'),
+  //     priority: PREFERENCES_PRIORITY,
+  //   },
+  //   {
+  //     id: 'preferences/dark-theme',
+  //     name: t('command-palette.action.dark-theme', 'Dark'),
+  //     keywords: 'dark theme',
+  //     perform: () => changeTheme('dark'),
+  //     parent: 'preferences/theme',
+  //     priority: PREFERENCES_PRIORITY,
+  //   },
+  //   {
+  //     id: 'preferences/light-theme',
+  //     name: t('command-palette.action.light-theme', 'Light'),
+  //     keywords: 'light theme',
+  //     perform: () => changeTheme('light'),
+  //     parent: 'preferences/theme',
+  //     priority: PREFERENCES_PRIORITY,
+  //   },
+  // ];
 
-  const extensionActions = getExtensionActions();
+  // const extensionActions = getExtensionActions();
   const navBarActions = navTreeToActions(navBarTree);
 
   return [...navBarActions];
