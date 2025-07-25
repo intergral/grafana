014712f9d3702e4db9496f4aa05306caefc569e2
dan <danhodgson@hotmail.co.uk>
2025-07-25 13:18:55 +0100

    feat: remove profile from topbar

--- public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
+++ public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
@@ -1,31 +1,17 @@
 import { css } from '@emotion/css';
-import React, { memo } from 'react';
+import { memo } from 'react';
 
 import { GrafanaTheme2, NavModelItem } from '@grafana/data';
 import { Components } from '@grafana/e2e-selectors';
-import { t } from '@grafana/i18n';
 import { ScopesContextValue } from '@grafana/runtime';
-import { Icon, Stack, ToolbarButton, useStyles2 } from '@grafana/ui';
-import { config } from 'app/core/config';
-import { MEGA_MENU_TOGGLE_ID } from 'app/core/constants';
+import { Stack, useStyles2 } from '@grafana/ui';
 import { useGrafana } from 'app/core/context/GrafanaContext';
-import { contextSrv } from 'app/core/core';
-import { useMediaQueryMinWidth } from 'app/core/hooks/useMediaQueryMinWidth';
 import { HOME_NAV_ID } from 'app/core/reducers/navModel';
-import { useSelector } from 'app/types/store';
+import { useSelector } from 'app/types';
 
-import { Branding } from '../../Branding/Branding';
 import { Breadcrumbs } from '../../Breadcrumbs/Breadcrumbs';
 import { buildBreadcrumbs } from '../../Breadcrumbs/utils';
-import { ExtensionToolbarItem } from '../ExtensionSidebar/ExtensionToolbarItem';
-import { HistoryContainer } from '../History/HistoryContainer';
-import { NavToolbarSeparator } from '../NavToolbar/NavToolbarSeparator';
-import { QuickAdd } from '../QuickAdd/QuickAdd';
 
-import { HelpTopBarButton } from './HelpTopBarButton';
-import { InviteUserButton } from './InviteUserButton';
-import { ProfileButton } from './ProfileButton';
-import { SignInLink } from './SignInLink';
 import { SingleTopBarActions } from './SingleTopBarActions';
 import { TopSearchBarCommandPaletteTrigger } from './TopSearchBarCommandPaletteTrigger';
 import { getChromeHeaderLevelHeight } from './useChromeHeaderHeight';
@@ -55,29 +41,13 @@ export const SingleTopBar = memo(function SingleTopBar({
   const state = chrome.useState();
   const menuDockedAndOpen = !state.chromeless && state.megaMenuDocked && state.megaMenuOpen;
   const styles = useStyles2(getStyles, menuDockedAndOpen);
-  const profileNode = useSelector((state) => state.navIndex['profile']);
   const homeNav = useSelector((state) => state.navIndex)[HOME_NAV_ID];
   const breadcrumbs = buildBreadcrumbs(sectionNav, pageNav, homeNav);
-  const unifiedHistoryEnabled = config.featureToggles.unifiedHistory;
-  const isSmallScreen = !useMediaQueryMinWidth('sm');
 
   return (
     <>
       <div className={styles.layout}>
         <Stack minWidth={0} gap={0.5} alignItems="center" flex={{ xs: 2, lg: 1 }}>
-          {!menuDockedAndOpen && (
-            <ToolbarButton
-              narrow
-              id={MEGA_MENU_TOGGLE_ID}
-              onClick={onToggleMegaMenu}
-              tooltip={t('navigation.megamenu.open', 'Open menu')}
-            >
-              <Stack gap={0} alignItems="center">
-                <Branding.MenuLogo className={styles.img} />
-                <Icon size="sm" name="angle-down" />
-              </Stack>
-            </ToolbarButton>
-          )}
           <Breadcrumbs breadcrumbs={breadcrumbs} className={styles.breadcrumbsWrapper} />
           {!showToolbarLevel && breadcrumbActions}
         </Stack>
@@ -91,15 +61,6 @@ export const SingleTopBar = memo(function SingleTopBar({
           minWidth={{ xs: 'unset', lg: 0 }}
         >
           <TopSearchBarCommandPaletteTrigger />
-          {unifiedHistoryEnabled && !isSmallScreen && <HistoryContainer />}
-          {!isSmallScreen && <QuickAdd />}
-          <HelpTopBarButton isSmallScreen={isSmallScreen} />
-          <NavToolbarSeparator />
-          {!isSmallScreen && <ExtensionToolbarItem compact={isSmallScreen} />}
-          {!showToolbarLevel && actions}
-          {!contextSrv.user.isSignedIn && <SignInLink />}
-          <InviteUserButton />
-          {profileNode && <ProfileButton profileNode={profileNode} onToggleKioskMode={onToggleKioskMode} />}
         </Stack>
       </div>
       {showToolbarLevel && (
