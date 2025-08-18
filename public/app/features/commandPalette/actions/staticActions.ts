29b99a76bec88ceef0ca89094903740a1d487765
dan <danhodgson@hotmail.co.uk>
2025-08-18 11:09:03 +0100

    fix: remove navtree items

--- public/app/features/commandPalette/actions/staticActions.ts
+++ public/app/features/commandPalette/actions/staticActions.ts
@@ -1,18 +1,10 @@
-import { useMemo } from 'react';
-
 import { NavModelItem } from '@grafana/data';
-import { t } from '@grafana/i18n';
-import { getEnrichedHelpItem } from 'app/core/components/AppChrome/MegaMenu/utils';
-import {
-  shouldRenderInviteUserButton,
-  performInviteUserClick,
-} from 'app/core/components/AppChrome/TopBar/InviteUserButtonUtils';
-import { changeTheme } from 'app/core/services/theme';
-import { currentMockApiState, toggleMockApiAndReload, togglePseudoLocale } from 'app/dev-utils';
-import { useSelector } from 'app/types/store';
+import { enrichHelpItem } from 'app/core/components/AppChrome/MegaMenu/utils';
+import { performInviteUserClick, shouldRenderInviteUserButton } from 'app/core/components/InviteUserButton/utils';
+import { t } from 'app/core/internationalization';
 
 import { CommandPaletteAction } from '../types';
-import { ACTIONS_PRIORITY, DEFAULT_PRIORITY, PREFERENCES_PRIORITY } from '../values';
+import { ACTIONS_PRIORITY, DEFAULT_PRIORITY } from '../values';
 
 // TODO: Clean this once ID is mandatory on nav items
 function idForNavItem(navItem: NavModelItem) {
@@ -25,7 +17,7 @@ function navTreeToActions(navTree: NavModelItem[], parents: NavModelItem[] = [])
   for (let navItem of navTree) {
     // help node needs enriching with the frontend links
     if (navItem.id === 'help') {
-      navItem = getEnrichedHelpItem({ ...navItem });
+      navItem = enrichHelpItem({ ...navItem });
       delete navItem.url;
     }
     const { url, target, text, isCreateAction, children, onClick, keywords } = navItem;
@@ -78,81 +70,8 @@ function navTreeToActions(navTree: NavModelItem[], parents: NavModelItem[] = [])
   return navActions;
 }
 
-function getGlobalActions(): CommandPaletteAction[] {
-  const actions: CommandPaletteAction[] = [
-    {
-      id: 'preferences/theme',
-      name: t('command-palette.action.change-theme', 'Change theme'),
-      keywords: 'interface color dark light',
-      section: t('command-palette.section.preferences', 'Preferences'),
-      priority: PREFERENCES_PRIORITY,
-    },
-    {
-      id: 'preferences/dark-theme',
-      name: t('command-palette.action.dark-theme', 'Dark'),
-      keywords: 'dark theme',
-      perform: () => changeTheme('dark'),
-      parent: 'preferences/theme',
-      priority: PREFERENCES_PRIORITY,
-    },
-    {
-      id: 'preferences/light-theme',
-      name: t('command-palette.action.light-theme', 'Light'),
-      keywords: 'light theme',
-      perform: () => changeTheme('light'),
-      parent: 'preferences/theme',
-      priority: PREFERENCES_PRIORITY,
-    },
-  ];
+export default (navBarTree: NavModelItem[], extensionActions: CommandPaletteAction[]): CommandPaletteAction[] => {
+  const navBarActions = navTreeToActions(navBarTree);
 
-  if (process.env.NODE_ENV === 'development') {
-    // eslint-disable @grafana/i18n/no-untranslated-strings
-    const section = 'Dev tooling';
-    const currentState = currentMockApiState();
-    const mockApiAction = currentState ? 'Disable' : 'Enable';
-    actions.push({
-      id: 'preferences/dev/toggle-mock-api',
-      section,
-      name: `${mockApiAction} Mock API worker and reload`,
-      subtitle: 'Intercepts requests and returns mock data using MSW',
-      keywords: 'mock api',
-      priority: PREFERENCES_PRIORITY,
-      perform: toggleMockApiAndReload,
-    });
-
-    actions.push({
-      id: 'preferences/dev/pseudo-locale',
-      section,
-      name: 'Toggle pseudo locale',
-      subtitle: 'Toggles between default language and pseudo locale',
-      keywords: 'pseudo locale',
-      priority: PREFERENCES_PRIORITY,
-      perform: () => {
-        togglePseudoLocale();
-      },
-    });
-    // eslint-enable @grafana/i18n/no-untranslated-strings
-  }
-
-  return actions;
-}
-
-export function useStaticActions(): CommandPaletteAction[] {
-  const navBarTree = useSelector((state) => state.navBarTree);
-  return useMemo(() => {
-    const navBarActions = navTreeToActions(navBarTree);
-
-    if (shouldRenderInviteUserButton()) {
-      navBarActions.push({
-        id: 'invite-user',
-        name: t('navigation.invite-user.invite-new-user-button', 'Invite new user'),
-        section: t('command-palette.section.actions', 'Actions'),
-        priority: ACTIONS_PRIORITY,
-        perform: () => {
-          performInviteUserClick('command_palette_actions', 'invite-user-command-palette');
-        },
-      });
-    }
-    return [...getGlobalActions(), ...navBarActions];
-  }, [navBarTree]);
-}
+  return [...extensionActions, ...navBarActions];
+};
