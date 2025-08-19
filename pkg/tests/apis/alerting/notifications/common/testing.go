efd5a8d8776a092ef8abb8699c5738a6b149d8f2
dan <danhodgson@hotmail.co.uk>
2025-08-19 09:54:05 +0100

    fix(tests): update default namespace for test

--- pkg/tests/apis/alerting/notifications/common/testing.go
+++ pkg/tests/apis/alerting/notifications/common/testing.go
@@ -3,58 +3,89 @@ package common
 import (
 	"testing"
 
+	"github.com/grafana/grafana/pkg/tests/apis"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 
-	"github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/alertingnotifications/v0alpha1"
-	"github.com/grafana/grafana/pkg/tests/apis"
+	v0alpha1_receiver "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/resource/receiver/v0alpha1"
+	v0alpha1_routingtree "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/resource/routingtree/v0alpha1"
+	v0alpha1_templategroup "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/resource/templategroup/v0alpha1"
+	v0alpha1_timeinterval "github.com/grafana/grafana/apps/alerting/notifications/pkg/apis/resource/timeinterval/v0alpha1"
 )
 
-func NewReceiverClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1.Receiver, v0alpha1.ReceiverList] {
+func NewReceiverClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1_receiver.Receiver, v0alpha1_receiver.ReceiverList] {
 	t.Helper()
 
 	client, err := dynamic.NewForConfig(user.NewRestConfig())
 	require.NoError(t, err)
 
-	return &apis.TypedClient[v0alpha1.Receiver, v0alpha1.ReceiverList]{
+	// Use the user's namespace instead of hardcoded "default"
+	namespace := user.Identity.GetNamespace()
+
+	return &apis.TypedClient[v0alpha1_receiver.Receiver, v0alpha1_receiver.ReceiverList]{
 		Client: client.Resource(
-			v0alpha1.ReceiverKind().GroupVersionResource()).
-			Namespace("default"),
+			schema.GroupVersionResource{
+				Group:    v0alpha1_receiver.Kind().Group(),
+				Version:  v0alpha1_receiver.Kind().Version(),
+				Resource: v0alpha1_receiver.Kind().Plural(),
+			}).Namespace(namespace),
 	}
 }
 
-func NewRoutingTreeClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1.RoutingTree, v0alpha1.RoutingTreeList] {
+func NewRoutingTreeClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1_routingtree.RoutingTree, v0alpha1_routingtree.RoutingTreeList] {
 	t.Helper()
 
 	client, err := dynamic.NewForConfig(user.NewRestConfig())
 	require.NoError(t, err)
 
-	return &apis.TypedClient[v0alpha1.RoutingTree, v0alpha1.RoutingTreeList]{
+	// Use the user's namespace instead of hardcoded "default"
+	namespace := user.Identity.GetNamespace()
+
+	return &apis.TypedClient[v0alpha1_routingtree.RoutingTree, v0alpha1_routingtree.RoutingTreeList]{
 		Client: client.Resource(
-			v0alpha1.RoutingTreeKind().GroupVersionResource()).Namespace("default"),
+			schema.GroupVersionResource{
+				Group:    v0alpha1_routingtree.Kind().Group(),
+				Version:  v0alpha1_routingtree.Kind().Version(),
+				Resource: v0alpha1_routingtree.Kind().Plural(),
+			}).Namespace(namespace),
 	}
 }
 
-func NewTemplateGroupClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1.TemplateGroup, v0alpha1.TemplateGroupList] {
+func NewTemplateGroupClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1_templategroup.TemplateGroup, v0alpha1_templategroup.TemplateGroupList] {
 	t.Helper()
 
 	client, err := dynamic.NewForConfig(user.NewRestConfig())
 	require.NoError(t, err)
 
-	return &apis.TypedClient[v0alpha1.TemplateGroup, v0alpha1.TemplateGroupList]{
+	// Use the user's namespace instead of hardcoded "default"
+	namespace := user.Identity.GetNamespace()
+
+	return &apis.TypedClient[v0alpha1_templategroup.TemplateGroup, v0alpha1_templategroup.TemplateGroupList]{
 		Client: client.Resource(
-			v0alpha1.TemplateGroupKind().GroupVersionResource()).Namespace("default"),
+			schema.GroupVersionResource{
+				Group:    v0alpha1_templategroup.Kind().Group(),
+				Version:  v0alpha1_templategroup.Kind().Version(),
+				Resource: v0alpha1_templategroup.Kind().Plural(),
+			}).Namespace(namespace),
 	}
 }
 
-func NewTimeIntervalClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1.TimeInterval, v0alpha1.TimeIntervalList] {
+func NewTimeIntervalClient(t *testing.T, user apis.User) *apis.TypedClient[v0alpha1_timeinterval.TimeInterval, v0alpha1_timeinterval.TimeIntervalList] {
 	t.Helper()
 
 	client, err := dynamic.NewForConfig(user.NewRestConfig())
 	require.NoError(t, err)
 
-	return &apis.TypedClient[v0alpha1.TimeInterval, v0alpha1.TimeIntervalList]{
+	// Use the user's namespace instead of hardcoded "default"
+	namespace := user.Identity.GetNamespace()
+
+	return &apis.TypedClient[v0alpha1_timeinterval.TimeInterval, v0alpha1_timeinterval.TimeIntervalList]{
 		Client: client.Resource(
-			v0alpha1.TimeIntervalKind().GroupVersionResource()).Namespace("default"),
+			schema.GroupVersionResource{
+				Group:    v0alpha1_timeinterval.Kind().Group(),
+				Version:  v0alpha1_timeinterval.Kind().Version(),
+				Resource: v0alpha1_timeinterval.Kind().Plural(),
+			}).Namespace(namespace),
 	}
 }
